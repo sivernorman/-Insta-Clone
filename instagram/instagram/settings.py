aab631fc434d344eeba4bae7925d53000e560db0
sivernorman <silvernorman.o@gmail.com>
2022-03-05 15:59:46 +0300

    updated templets folder

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from msilib.schema import Media
 from pathlib import Path
+from re import M
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL='media'
+MEDIA_ROOT=os.path.join(BASE_DIR_.'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
