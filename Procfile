c0fce44a0fe69572cfd9a675fa1ac062592d5a67
sivernorman <silvernorman.o@gmail.com>
2022-03-09 22:58:50 +0300

    deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn insta-clone.wsgi --log-file -
+web: gunicorn instagram.wsgi --log-file -
--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -27,8 +27,7 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
+DEBUG = os.environ.get('DEBUG', False) 
 ALLOWED_HOSTS = []
 
 
